af0e49da143f8accc8940c5ae815a08e5391a3a7
Tunnox <chukwuemekaumunna@gmail.com>
2025-08-06 16:41:54 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/static/public/<path:filename>')
-def serve_public_file(filename):
-    return send_from_directory('static/public', filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
